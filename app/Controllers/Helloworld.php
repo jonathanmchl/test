eae0a69a64a4f43c0a5813060cb91e0fd896cfa2
jonathanmchl <jonathanmworks@gmail.com>
2020-11-13 22:46:55 +0700

    removed mylogfile.log

---
+++ app/Controllers/Helloworld.php
@@ -0,0 +1,12 @@
+<?php namespace App\Controllers;
+
+class Helloworld extends BaseController
+{
+	public function index()
+	{
+		echo "hello joni";
+	}
+
+	//--------------------------------------------------------------------
+
+}
